57b4df68ba4a10de65332078c95b281e620085a5
James Armes (they/them) <jarmes@codeforamerica.org>
2025-06-24 01:43:08 -0400

    feat: Added df-all-screens-scrape application. (#11)

--- tofu/config/development/infra/local.tf
+++ tofu/config/development/infra/local.tf
@@ -1,5 +1,6 @@
 locals {
   apps = {
     dc-sebt-portal : yamldecode(file("${path.module}/apps/dc-sebt-portal.yaml"))
+    df-all-screen-scrapes : yamldecode(file("${path.module}/apps/df-all-screens-scrape.yaml"))
   }
 }
