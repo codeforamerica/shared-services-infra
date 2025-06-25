32c89aa36a0c4ce346fd8b4bcd7f9aac77923adb
James Armes <jarmes@codeforamerica.org>
2025-06-25 18:20:41 -0400

    feat: Added column-sandbox application.

--- tofu/config/development/infra/local.tf
+++ tofu/config/development/infra/local.tf
@@ -1,5 +1,6 @@
 locals {
   apps = {
+    column-sandbox : yamldecode(file("${path.module}/apps/column-sandbox.yaml"))
     dc-sebt-portal : yamldecode(file("${path.module}/apps/dc-sebt-portal.yaml"))
     df-all-screens-scrape : yamldecode(file("${path.module}/apps/df-all-screens-scrape.yaml"))
   }
