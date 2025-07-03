81446c3512f1ec30eb72c3343c2b73d916fb129e
James Armes <jarmes@codeforamerica.org>
2025-07-03 16:06:18 -0400

    feat: Added a redirect from the old dev documentation domain.

--- tofu/config/development/docs/outputs.tf
+++ tofu/config/development/docs/outputs.tf
@@ -1,4 +1,14 @@
+output "certificate_validation_records" {
+  description = "The DNS records required to validate the ACM certificate."
+  value       = module.redirect.certificate_validation_records
+}
+
 output "endpoint_url" {
   description = "The URL of the documentation endpoint."
   value       = module.docs.endpoint_url
+}
+
+output "redirect_dns" {
+  description = "DNS information for the CloudFront redirect needed to create DNS records."
+  value       = module.redirect.cloudfront_dns
 }
